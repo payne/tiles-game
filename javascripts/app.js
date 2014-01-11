253e0a7b35923edb7569f25a02d32f71231ad49c
Willson Mock <willson.mock@gmail.com>
2014-01-10 22:42:33 -0500

    Add isValidMove and swap

--- javascripts/app.js
+++ javascripts/app.js
@@ -14,3 +14,22 @@ Tiles.prototype.legalMoves = {
     "r3c2": ["r3c1", "r2c2", "r3c3"],
     "r3c3": ["r2c3", "r3c2"]
 };
+
+Tiles.prototype.isValidMove = function (from, to) {
+    // from and to are string inputs
+    return _.contains(this.legalMoves[from], to);
+};
+
+Tiles.prototype.swap = function (from, to) {
+    var copy;
+    if (this.isValidMove(from, to) &&  this.isSwapWithNull(from, to)) {
+        copy = _.reduce(this.setup, function (acc, val, prop, thisObj) {
+            // this should point to the Tile object, while thisObj points to the Tile object's setup property
+            acc[prop] = val;
+            return acc;
+        }, {}, this);
+        copy[from] = this.setup[to];
+        copy[to] = this.setup[from];
+        return new Tiles(copy);
+    }
+};
